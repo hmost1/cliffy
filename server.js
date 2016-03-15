244fb2a6603d84ceaeb5394a856a47fa19115d57
hmost1 <haleymost1@gmail.com>
2016-03-15 12:27:31 -0700

    remove hardcoding cases

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+// get an instance of the express Router
+var router = express.Router();  
 
 // middleware called with all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Call made');
     next(); // make sure we go to the next routes
 });
 
@@ -35,40 +35,20 @@ router.use(function(req, res, next) {
 router.post('/', function(req, res) {
 	var text = req.body.text;
 
-	s3.listObjects({Bucket: "cliffy"}, function(err, data){
+	s3.listObjects({Bucket: "cliffy", EncodingType: "url"}, function(err, data){
+    //pick a random item in this bucket
 		var random = Math.floor((Math.random() * data.Contents.length));
-		var url = s3.getSignedUrl('getObject', {Bucket: "cliffy", Key: data.Contents[random].Key});
-		res.header("Content-Type", "application/json");
-		switch(text){
-			case "1":
-				res.status(200).json({response_type: "in_channel", text: "welcome to cliffy", attachments: [{text: "1", image_url: url}]});
-				break;
-  			case "2":
-  				url = "https://cliffy.s3.amazonaws.com/giphy.gif";
-  			  	res.status(200).json({response_type: "in_channel", text: "welcome to cliffy", attachments: [{text: "2", image_url: url}]});
-  			  	break;
-  			case "3":
-  				url = "http://i.giphy.com/3o6UBo8U2iHUG5mszS.gif"
-  				res.status(200).json({response_type: "in_channel", text: "welcome to cliffy", attachments: [{text: "3", image_url: url}]});
-  				break;
-  			case "4":
-  				res.status(200).json({response_type: "in_channel", text: "welcome to cliffy", attachments: [{image_url: url}]});
-  				break;
-  			default:
-  				res.status(200).json({response_type: "in_channel", attachments: [{text: "cliffy", image_url: url}]});
-				break;
-		}
+
+    //TODO: is there an api call to check if this item is public and then make it public if not?
+    //for now we have to manually make them public in the s3 console 
+    //TODO: shouldn't have this hardcoded, use get public url if possible 
+    var key = data.Contents[random].Key; 
+		var url = "https://s3-us-west-1.amazonaws.com/cliffy/" + data.Contents[random].Key; 
+
+    res.header("Content-Type", "application/json");
+		res.status(200).json({response_type: "in_channel", attachments: [{text: "cliffy", image_url: url}]});
 	});
 });
-
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api with a get!' });   
-});
-
-router.put('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api with a put!' });   
-});
-
 
 //the REST (lol) of the routes go here 
 
