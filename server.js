57692141baf51798dbadd90b5f2ed2a71f10f67d
hmost1 <haleymost1@gmail.com>
2016-03-23 15:42:32 -0700

    Clean up, add env vars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// pull in all the packages we'll need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+// pull in all the packages we use
+var express    = require('express');      
+var app        = express();                 
 var bodyParser = require('body-parser');
 var AWS = require('aws-sdk');        
 
@@ -12,48 +12,58 @@ AWS.config.update({
 });
 
 var s3 = new AWS.S3();
+var bucket = process.env.S3_BUCKET;
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-// get an instance of the express Router
+var port = process.env.PORT || 8080; 
 var router = express.Router();  
 
-// middleware called with all requests
+// middleware for all requests
 router.use(function(req, res, next) {
-    // do logging
-    next(); // make sure we go to the next routes
+	var token = req.body.token; 
+	var user = req.body.user_id
+	
+	switch (token)
+	{
+		case process.env.LADIES_TOKEN: 
+				next();
+				break; 
+		case process.env.IDI_TOKEN:
+			if(process.env.IDI_NAMES && process.env.IDI_NAMES.includes(user)){
+				next();
+				break;
+			} //else, continue on to the default
+		default:
+			res.header("Content-Type", "application/json");
+			res.status(200).json({response_type: "in_channel", text: "You don't currently have access to cliffy,"
+			+ " check with haley. If you don't know who that is, you're in the wrong place."});
+	}
 });
 
 // get random object in s3 bucket
 router.post('/', function(req, res) {
 	var text = req.body.text;
 
-	s3.listObjects({Bucket: "cliffy", EncodingType: "url"}, function(err, data){
-    //pick a random item in this bucket
+	s3.listObjects({Bucket: bucket, EncodingType: "url"}, function(err, data){
+    	//pick a random item in this bucket
 		var random = Math.floor((Math.random() * data.Contents.length));
-
-    //TODO: is there an api call to check if this item is public and then make it public if not?
-    //for now we have to manually make them public in the s3 console 
-    //TODO: shouldn't have this hardcoded, use get public url if possible 
-    var key = data.Contents[random].Key; 
-		var url = "https://s3-us-west-1.amazonaws.com/cliffy/" + data.Contents[random].Key; 
-
-    res.header("Content-Type", "application/json");
+	
+    	//TODO: is there an api call to check if this item is public and then make it public if not?
+    	//for now we have to manually make them public in the s3 console 
+    	//TODO: shouldn't have this hardcoded, use get public url if possible 
+    	var key = data.Contents[random].Key; 
+		var url = "https://s3-us-west-1.amazonaws.com/" + bucket + "/" + data.Contents[random].Key; 
+	
+    	res.header("Content-Type", "application/json");
 		res.status(200).json({response_type: "in_channel", attachments: [{text: "cliffy", image_url: url}]});
 	});
 });
 
-//the REST (lol) of the routes go here 
+//the REST (lol) of the routes will go here 
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+//access this set of routes through the app under the home '/'
 app.use('/', router);
 
 // START THE SERVER
